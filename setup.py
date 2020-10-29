fe534fa93063dde48be0ce0007aa3234e2143439
Sushant Sharma <sushantsharma@chegg.com>
2020-10-29 13:05:10 +0530

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("./README.rst") as f:
     readme = f.read()
 
 setup(
-    version="1.1.0",
+    version="1.1.1",
     name="axe-selenium-python-dev",
     use_scm_version=False,
     setup_requires=["setuptools_scm"],
